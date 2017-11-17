1d8cfacce2c018d1d6f0b57e4b96c5b32bd694fe
Ben Smith <benjaminsmith1981@gmail.com>
2017-11-17 17:24:34 +0100

    Added alamofire

---
+++ ARKitNavigationDemo/Pods/Target Support Files/Pods-ARKitNavigationDemo/Pods-ARKitNavigationDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARKitNavigationDemo : NSObject
+@end
+@implementation PodsDummy_Pods_ARKitNavigationDemo
+@end
---
+++ ARKitNavigationDemo/Pods/Target Support Files/Pods-ARKitNavigationDemoUITests/Pods-ARKitNavigationDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARKitNavigationDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ARKitNavigationDemoUITests
+@end
